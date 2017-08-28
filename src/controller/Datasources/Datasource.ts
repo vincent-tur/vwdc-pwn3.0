f2496eca83ee61b1f053ccbbb6ae292347b694f5
vincent-tur <vincentturato@gmail.com>
2017-08-28 00:57:41 -0700

    trying to do it by pages

--- src/controller/Datasources/Datasource.ts
+++ src/controller/Datasources/Datasource.ts
@@ -1,3 +1,5 @@
+
+
 export class Datasource {
     dataObj: any;
     dataUrlParams: {[paramName: string] : string};
@@ -46,6 +48,43 @@ export class Datasource {
                 // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
                 // console.log('body:', body); // Print the HTML for the Google homepage.
             });
+        });
+    }
+    getDataNEW(start: number, iterateBy: number, end: number, curPage: any) {
+        var that = this;
+
+        return new Promise(function (fulfill, reject){
+            const { URL, URLSearchParams } = require('url');
+            var request = require('request');
+            var urlStr = that.getURL();
+            console.log(urlStr.url);
+            var newUrlObj = URL.parse(urlStr.url);
+            var newSearchObj = new URLSearchParams(newUrlObj.search);
+
+            newUrlObj.searchParams.append(curPage.urlParamName, curPage.value);
+            var newUrlStr = newUrlObj.href;
+
+            var loopCounter = 0;
+            for(var i: number = start; i < end; i+=iterateBy){
+                request(newUrlStr, function (error: any, response: any, body: any) {
+                    that.dataObj[loopCounter] = JSON.parse(body);
+                    that.formatDataObj();
+                    if(that.dataObj == {}){
+                        reject("Received no data. URL attempt: " + newUrlStr)
+                    }else{
+                        fulfill({
+                            data: that.dataObj,
+                            url: newUrlStr
+                        });
+                    }
+
+                    // console.log('error:', error); // Print the error if one occurred
+                    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                    // console.log('body:', body); // Print the HTML for the Google homepage.
+                loopCounter++;
+                });
+            }
+
         });
     }
 
--- src/controller/Datasources/Toggl/Toggl.ts
+++ src/controller/Datasources/Toggl/Toggl.ts
@@ -31,42 +31,37 @@ export default class Toggl extends Datasource{
     }
 
 
-    // get_pages(){
-    //     var url = "https://toggl.com/reports/api/v2/details?user_agent="+email+"&workspace_id="+workspace+"&since="+startt+"&until="+endt;
-    //
-    //     var that = this;
-    //
-    //     return new Promise(function (fulfill, reject){
-    //         var request = require('request');
-    //         var url = that.getURL();
-    //         request(url, function (error: any, response: any, body: any) {
-    //             that.dataObj[0] = JSON.parse(body);
-    //             that.formatDataObj();
-    //             if(that.dataObj == {}){
-    //                 reject("Received no data. URL attempt: " + url)
-    //             }else{
-    //                 fulfill({
-    //                     data: that.dataObj,
-    //                     url: url
-    //                 });
-    //             }
-    //
-    //             // console.log('error:', error); // Print the error if one occurred
-    //             // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    //             // console.log('body:', body); // Print the HTML for the Google homepage.
-    //         });
-    //     });
-    //
-    //     $.ajax({
-    //         type: 'GET',
-    //         url: url,
-    //         dataType: 'json',
-    //         beforeSend: function (xhr) {
-    //             xhr.setRequestHeader('Authorization', 'Basic ' + btoa(token + ':api_token'));
-    //         },
-    //         // success: pagesCallback
-    //     });
-    // }
+
+    getData(){
+
+        var url = this.dataUrlBase + "?user_agent="+this.dataUrlParams['email']+"&workspace_id="+this.dataUrlParams['workspace']+"&since="+this.dataUrlParams['startt']+"&until="+this.dataUrlParams['endt'];
+
+            var that = this;
+
+        return new Promise(function (fulfill, reject){
+            var request = require('request');
+            var url = that.getURL();
+            request(url, function (error: any, response: any, body: any) {
+                var responseObj = JSON.parse(body);
+                var totalEntries = responseObj["total_count"];
+                var pages = String(Math.ceil(totalEntries/50));
+                that.getDataNEW(0,50, totalEntries, {urlParamName: 'page', value: pages})
+                // console.log('error:', error); // Print the error if one occurred
+                // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                // console.log('body:', body); // Print the HTML for the Google homepage.
+            });
+        });
+        //
+        // $.ajax({
+        //     type: 'GET',
+        //     url: url,
+        //     dataType: 'json',
+        //     beforeSend: function (xhr) {
+        //         xhr.setRequestHeader('Authorization', 'Basic ' + btoa(token + ':api_token'));
+        //     },
+        //     // success: pagesCallback
+        // });
+    }
     formatDataObj(){
         this.dataObj = this.dataObj[0].data;
     }
