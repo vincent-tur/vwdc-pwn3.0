f019edacdbc964ee0a306768fe979969e5d48d8f
vincent-tur <vincentturato@gmail.com>
2017-08-28 00:05:37 -0700

    trying to iterate get req

--- src/controller/Datasources/Toggl/Toggl.ts
+++ src/controller/Datasources/Toggl/Toggl.ts
@@ -30,6 +30,43 @@ export default class Toggl extends Datasource{
         return urlObj;
     }
 
+
+    // get_pages(){
+    //     var url = "https://toggl.com/reports/api/v2/details?user_agent="+email+"&workspace_id="+workspace+"&since="+startt+"&until="+endt;
+    //
+    //     var that = this;
+    //
+    //     return new Promise(function (fulfill, reject){
+    //         var request = require('request');
+    //         var url = that.getURL();
+    //         request(url, function (error: any, response: any, body: any) {
+    //             that.dataObj[0] = JSON.parse(body);
+    //             that.formatDataObj();
+    //             if(that.dataObj == {}){
+    //                 reject("Received no data. URL attempt: " + url)
+    //             }else{
+    //                 fulfill({
+    //                     data: that.dataObj,
+    //                     url: url
+    //                 });
+    //             }
+    //
+    //             // console.log('error:', error); // Print the error if one occurred
+    //             // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //             // console.log('body:', body); // Print the HTML for the Google homepage.
+    //         });
+    //     });
+    //
+    //     $.ajax({
+    //         type: 'GET',
+    //         url: url,
+    //         dataType: 'json',
+    //         beforeSend: function (xhr) {
+    //             xhr.setRequestHeader('Authorization', 'Basic ' + btoa(token + ':api_token'));
+    //         },
+    //         // success: pagesCallback
+    //     });
+    // }
     formatDataObj(){
         this.dataObj = this.dataObj[0].data;
     }
